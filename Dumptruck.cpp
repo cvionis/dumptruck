188db8af47e45bd438f1133a34df34a3c7682246
cvionis <cvionis03@gmail.com>
2023-04-28 17:39:25 -0400

    Refactor display functions into a single function

--- Dumptruck.cpp
+++ Dumptruck.cpp
@@ -6,6 +6,9 @@
 #include <string>
 #include <vector>
 
+constexpr int LSB{ 1 };
+constexpr int MSB{ 0 };
+
 long getFileSize(const std::string& filename)
 {
 	// Struct containing status of the provided file
@@ -53,60 +56,51 @@ long readBytesFromFile(std::vector<unsigned char>& bytesBuffer, const std::strin
 	return bytesRead;
 }
 
-// Display bytes in big-endian hexadecimal format
-void displayBigEndian(const std::vector<unsigned char>& bytes, long byteCount)
+// Default hex dump: display bytes in endianness of user's system
+void displayDefault(const std::vector<unsigned char>& bytes, long byteCount, int rowSize)
 {
-	int rowSize{ 8 };
 	int address{ 0 };
+	int groupSize{ 2 };
+	int currentGroup{ 1 };
+	int currentByte{ 0 };
 
-	for (int i{ 0 }, j{ 1 }, k{ 0 }; i < byteCount; ++i, ++j, ++k)
+	while (currentGroup < byteCount)
 	{
-		if (k % 16 == 0)
+		// Print padded address signifying number of bytes read from beginning of file
+		if (currentByte % rowSize == 0)
 		{
-			std::cout << std::setfill('0') << std::setw(7) << std::hex << address << ' ';
-			address += rowSize * 2;
+			std::cout << std::hex
+				<< std::setfill('0') << std::setw(7) << std::hex << address << ' '
+				<< std::dec;
+			address += rowSize;
+		}
+		// Get low and high bytes for each group of *groupSize* bytes
+		int byteH{ static_cast<int>(bytes.at(currentGroup - 1)) };
+		int byteL{ static_cast<int>(bytes.at(currentGroup)) };
+
+		// Print two bytes from the dump in the specificed byte order (little-endian or big-endian)
+		if (getEndianness() == LSB)
+		{
+			
+			std::cout << std::setfill('0') << std::setw(2) << std::hex << byteL << std::dec;
+			std::cout << std::setfill('0') << std::setw(2) << std::hex << byteH << std::dec;
+		}
+		else if (getEndianness() == MSB)
+		{
+			std::cout << std::setfill('0') << std::setw(2) << std::hex << byteH << std::dec;
+			std::cout << std::setfill('0') << std::setw(2) << std::hex << byteL << std::dec;
 		}
 
-		int byte{ static_cast<int>(bytes.at(i)) };
-
-		std::cout << std::setfill('0') << std::setw(2) << std::hex << byte << std::dec;
-
-		// Formatting: add spaces between every two bytes and a newline every rowSize bytes
-		if (j % 2 == 0)
+		// Formatting: separate bytes by group size with a space and print a newline when rowSize bytes are reached
+		if (currentByte % groupSize == 0)
 			std::cout << ' ';
-		if (j % (rowSize * 2) == 0)
+		if ((currentByte + 2) % rowSize == 0) // Note: 2 is added to current byte number because it starts w/ value 0.
 			std::cout << '\n';
+
+		currentGroup += 1;
+		currentByte  += 2;
 	}
 }
-
-// Display bytes in little-endian hexadecimal format
-void displayLittleEndian(const std::vector<unsigned char>& bytes, long byteCount)
-{
-	int rowSize{ 8 };
-	int address{ 0 };
-
-	for (int i{ 1 }, j{ 1 }, k{ 0 }; i < byteCount; i += 2, ++j, ++k)
-	{
-		if (k % 8 == 0)
-		{
-			std::cout << std::setfill('0') << std::setw(7) << std::hex << address << ' ';
-			address += rowSize * 2;
-		}
-
-		int byteL{ static_cast<int>(bytes.at(i)) };
-		int byteH{ static_cast<int>(bytes.at(i-1)) };
-
-		std::cout << std::setfill('0') << std::setw(2) << std::hex << byteL << std::dec;
-		std::cout << std::setfill('0') << std::setw(2) << std::hex << byteH << std::dec;
-
-		// Formatting: add spaces between every two bytes and a newline every rowSize bytes
-		if (j % 1 == 0)
-			std::cout << ' ';
-		if (j % rowSize == 0)
-			std::cout << '\n';
-	}
-}
-
 void displayBytes(const std::vector<unsigned char>& bytes, long byteCount)
 {
 	/* If canonical option present :
@@ -115,7 +109,7 @@ void displayBytes(const std::vector<unsigned char>& bytes, long byteCount)
 		   print default (in which little or big endian is decided)
 	*/
 	//displayBigEndian(bytes, byteCount);
-	displayLittleEndian(bytes, byteCount);
+	displayDefault(bytes, byteCount, 16);
 }
 
 int main(int argc, char* argv[])
